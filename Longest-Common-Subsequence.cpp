796b072cc9ea0cead345d96a01a4b60f2fd5a9c7
vharsh97 <36531607+vharsh97@users.noreply.github.com>
2020-04-28 20:59:25 +0530

    Create Longest-Common-Subsequence.cpp

---
+++ Longest-Common-Subsequence.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int get_lcs(char *str1, char *str2, int len1, int len2)
+{
+    int i, j;
+    int lcs[len1+1][len2+1];
+
+    for(i = 0 ; i < len2; i++)
+        lcs[0][i] = 0;
+
+    for(i = 0; i <= len1; i++)
+        lcs[i][0]=0;
+
+
+    for(i = 1; i <= len1; i++) {
+        for(j = 1; j <= len2; j++) {
+            // When charatcers of both string match
+            if(str1[i-1] == str2[j-1])
+                lcs[i][j] = 1 + lcs[i-1][j-1];
+            else
+                lcs[i][j] = lcs[i-1][j] > lcs[i][j-1]  ? lcs[i-1][j] : lcs[i][j-1];
+        }
+    }
+
+    return lcs[len1][len2];
+}
+
+
+int main()
+{
+    char str1[50] = "lmnop";
+    char str2[50] = "lmmnoop";
+
+    int result;
+    result = get_lcs(str1, str2, strlen(str1), strlen(str2));
+    printf("LCS for supplied input is:\n");
+    printf("%d\n", result);
+
+    return 0;
+}
