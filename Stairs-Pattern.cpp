a86600e0de25a761f905a55ce79cfe1dc89b2d7b
vharsh97 <36531607+vharsh97@users.noreply.github.com>
2020-04-30 19:41:27 +0530

    Create Stairs-Pattern.cpp

---
+++ Stairs-Pattern.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+int main() {
+int l, st = 2, x, r, z, n, sp;
+std::cout << "enter Index ";
+std::cin >> x;
+z = x;
+for (r = 1; r <= x; r++) {
+z = z - 1;
+for (n = 1; n <= 2; n++) {
+for (sp = 1; sp <= z; sp++) {
+std::cout << " ";
+}
+for (l = 1; l <= st; l++) {
+std::cout << "*";
+}
+std::cout <<"\n";
+}
+st = st + 2;
+}}
